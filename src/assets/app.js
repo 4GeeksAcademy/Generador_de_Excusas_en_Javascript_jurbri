d646a10a357364c29cdb7138ffbc2072340ddf7b
JurBri <89058157+JurBri@users.noreply.github.com>
2024-04-15 07:13:45 +0000

    se crea el html, el app.js con la logica para crear excusas aleatorias, y el style para configurar el estilo.

---
+++ src/assets/app.js
@@ -0,0 +1,33 @@
+function generarExcusa() {
+  // Arreglos de palabras para la excusa
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [  "before the class","when I was sleeping","while I was exercising","during my lunch","while I was praying"];
+
+  // Generar un índice aleatorio para cada array
+  const whoRandom = Math.floor(Math.random() * who.length);
+  const actionRandom = Math.floor(Math.random() * action.length);
+  const whatRandom = Math.floor(Math.random() * what.length);
+  const whenRandom = Math.floor(Math.random() * when.length);
+
+  // Concatenar las palabras para formar la excusa
+  const excusa = `${who[whoRandom]} ${action[actionRandom]}${what[whatRandom]}${when[whenRandom]}.`;
+
+  return excusa;
+}
+
+// Función para mostrar la excusa en el HTML
+function mostrarExcusa() {
+  
+  // Obtener el elemento con el ID "excuse"
+  let excuseElement = document.getElementById("excuse");
+
+  // Generar una excusa aleatoria
+  let excuse = generarExcusa();
+
+  // Establecer la excusa como el contenido HTML del elemento
+  excuseElement.innerHTML = excuse;
+}
+
+onload = mostrarExcusa();
